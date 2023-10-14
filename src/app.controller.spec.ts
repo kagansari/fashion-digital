2c6edc89504d731137d3fa6fc33f42a8344a5f49
Kagan Sari <kaganalisari@gmail.com>
2023-10-14 11:31:12 +0300

    add evaluation endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,75 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+import * as path from "path";
+import * as fs from "fs";
 
-describe('AppController', () => {
+export const getMockFileStream = async (url: string) => {
+  switch (url) {
+    case "url1":
+      return fs.createReadStream(path.join(__dirname, "./politics_en.csv"));
+
+    case "url2":
+      return fs.createReadStream(path.join(__dirname, "./politics_en_2.csv"));
+
+    default:
+      return fs.createReadStream(path.join(__dirname, "./politics_en.csv"));
+  }
+};
+
+export const getAxiosMockFileStream = async (url: string) => {
+  switch (url) {
+    case "url1":
+      return {
+        data: fs.createReadStream(path.join(__dirname, "./politics_en.csv")),
+      };
+
+    case "url2":
+      return {
+        data: fs.createReadStream(path.join(__dirname, "./politics_en_2.csv")),
+      };
+
+    default:
+      return {
+        data: fs.createReadStream(path.join(__dirname, "./politics_en.csv")),
+      };
+  }
+};
+
+export const mockResultSingleUrl = {
+  mostSpeeches: null,
+  mostSecurity: "Alexander Abel",
+  leastWordy: "Caesare Collins",
+};
+
+export const mockResultMultiUrl = {
+  mostSpeeches: "Alexander Abel",
+  mostSecurity: "Caesare Collins",
+  leastWordy: "Alexander Abel",
+};
+
+describe("AppController", () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    jest
+      .spyOn(appController, "getFileStream")
+      .mockImplementation(getMockFileStream);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe("Fashion Digital – Recruitment Exercise Node.js", () => {
+    it("should calculate right with single URL", async () => {
+      const result = await appController.evaluation("url1");
+      expect(result).toStrictEqual(mockResultSingleUrl);
+    });
+
+    it("should calculate right with multiple URLs", async () => {
+      const result = await appController.evaluation(["url1", "url2"]);
+      expect(result).toStrictEqual(mockResultMultiUrl);
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,121 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { BadRequestException, Controller, Get, Query } from "@nestjs/common";
+import axios from "axios";
+import { Readable } from "stream";
+
+type EvaluationResult = {
+  // Which politician gave the most speeches in 2013?
+  mostSpeeches: string | null;
+  // Which politician gave the most speeches on the topic „Internal Security"?
+  mostSecurity: string | null;
+  // Which politician used the fewest words (in total)?
+  leastWordy: string | null;
+};
+
+type SpeakerMetric = {
+  speaker: string;
+  count2013: number;
+  countInternalSecurity: number;
+  totalWordCount: number;
+};
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getFileStream(url: string): Promise<Readable> {
+    const response = await axios.get(url, { responseType: "stream" });
+    const excelStream = response.data;
+
+    return excelStream;
+  }
+
+  @Get("evaluation")
+  async evaluation(
+    @Query("url") url: string | string[],
+  ): Promise<EvaluationResult> {
+    if (!url) {
+      throw new BadRequestException("URL is required");
+    }
+    const urls = Array.isArray(url) ? url : [url];
+    // Store related information for each speaker, update for every row
+    const speakerMetricMap: Record<string, SpeakerMetric> = {};
+
+    const excelReadPromises = urls.map(async (nextUrl) => {
+      const excelFileStream = await this.getFileStream(nextUrl);
+      // Read every row until the end of the stream
+      return new Promise<void>((resolve, reject) => {
+        excelFileStream.on("data", (chunk) => {
+          const rows = chunk.toString().split("\n");
+
+          rows.forEach((row, index) => {
+            if (row.trim() === "" || index === 0) return;
+            const [speaker, subject, dateStr, wordCountStr] = row.split(", ");
+            const year = new Date(dateStr).getUTCFullYear();
+
+            if (!speakerMetricMap[speaker]) {
+              speakerMetricMap[speaker] = {
+                speaker,
+                count2013: 0,
+                totalWordCount: 0,
+                countInternalSecurity: 0,
+              };
+            }
+
+            speakerMetricMap[speaker].totalWordCount += Number(wordCountStr);
+            if (year === 2013) {
+              speakerMetricMap[speaker].count2013 += 1;
+            }
+            if (subject === "Internal Security") {
+              speakerMetricMap[speaker].countInternalSecurity += 1;
+            }
+          });
+        });
+
+        excelFileStream.on("end", () => {
+          resolve();
+        });
+        excelFileStream.on("error", reject);
+      });
+    });
+
+    await Promise.all(excelReadPromises);
+
+    const speakerMetrics = Object.values(speakerMetricMap);
+
+    // Find the speaker who has maximum count2013
+    const mostSpeeches =
+      speakerMetrics.reduce((acc, metric) => {
+        if (metric.count2013 > (acc?.count2013 || 0)) {
+          return metric;
+        }
+        return acc;
+      }, null)?.speaker || null;
+
+    // Find the speaker who has maximum countInternalSecurity
+    const mostSecurity =
+      speakerMetrics.reduce((acc, metric) => {
+        if (metric.countInternalSecurity > (acc?.countInternalSecurity || 0)) {
+          return metric;
+        }
+        return acc;
+      }, null)?.speaker || null;
+
+    // Find the speaker who has minimum totalWordCount
+    const leastWordy =
+      speakerMetrics.reduce((acc, metric) => {
+        if (
+          metric.totalWordCount <
+          (acc?.totalWordCount || Number.MAX_SAFE_INTEGER)
+        ) {
+          return metric;
+        }
+        return acc;
+      }, null)?.speaker || null;
+
+    return {
+      mostSpeeches,
+      mostSecurity,
+      leastWordy,
+    };
   }
 }
